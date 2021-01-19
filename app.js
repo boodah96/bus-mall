b04212fda68eae4036de98ea8b0bb56d38497437
boodah96 <2014990065@ses.yu.edu.jo>
2021-01-20 00:23:28 +0200

    lab12

--- app.js
+++ app.js
@@ -94,17 +94,18 @@ renderThreeRandomImages();
 
 
 
-var firstProductImageIndex=-1;
-var secondProductImageIndex=-1;
-var thirdProductImageIndex=-1;
+var firstProductImageIndex=0;
+var secondProductImageIndex=0;
+var thirdProductImageIndex=0;
   
 function UserClick(event){
     
 
+console.log('AttemptsCounter :',AttemptsCounter+"/"+defaultMaxAttempts)
+console.log('befor',AttemptsCounter)
 
 
-if(AttemptsCounter < defaultMaxAttempts){
-
+if(AttemptsCounter <defaultMaxAttempts){
     console.log(typeof (event.target.id));
     if(event.target.id === 'firstProductImage'){
         
@@ -116,24 +117,20 @@ if(AttemptsCounter < defaultMaxAttempts){
         } 
         if(event.target.id === 'thirdProductImage'){
             Product.prototype.allProducts[thirdProductImageIndex].votes++;
-        } 
-        Product.prototype.allProducts[thirdProductImageIndex].shown++;
-        Product.prototype.allProducts[secondProductImageIndex].shown++;
-        Product.prototype.allProducts[firstProductImageIndex].shown++;
-            AttemptsCounter++;  
-            console.log('AttemptsCounter :',AttemptsCounter+"/"+defaultMaxAttempts)
 
-            if(AttemptsCounter == defaultMaxAttempts ){
-               document.getElementById ('ViewResults').removeAttribute('disabled')
-               divImages.removeEventListener('click',UserClick);
+        }   
+        AttemptsCounter++;   
 
-            }
-            if(AttemptsCounter < defaultMaxAttempts ){
-                renderThreeRandomImages(); 
- 
-             }
+        localStorage.setItem('counter',AttemptsCounter);
+        AttemptsCounter=JSON.parse(localStorage.getItem('counter'))
 
+     localStorage.setItem('allProduct',JSON.stringify(Product.prototype.allProducts))
+     renderThreeRandomImages(); 
+     if(AttemptsCounter ==(defaultMaxAttempts)){ 
+        document.getElementById('ViewResults').removeAttribute("disabled");
+        divImages.removeEventListener('click',UserClick);
 
+}
 
     }
 else    {
@@ -151,20 +148,15 @@ var lastRoundFirst=-3;
 var lastRoundSecond=-2;
 var lastRoundThird=-1;
 function renderThreeRandomImages(){
-
-
-   
-   
-
-
-    var lastClick=[lastRoundFirst,lastRoundSecond,lastRoundThird];
-
+    
+    
+        var lastClick=[lastRoundFirst,lastRoundSecond,lastRoundThird];
+        if(AttemptsCounter <(defaultMaxAttempts-1)){
     do{
         firstProductImageIndex = generateRandomIndex();
     } while( lastClick.includes(firstProductImageIndex));
     lastRoundFirst=firstProductImageIndex;
     lastClick.push(firstProductImageIndex);
-
   
     do{
         secondProductImageIndex = generateRandomIndex();
@@ -178,13 +170,16 @@ function renderThreeRandomImages(){
     } while(lastClick.includes(thirdProductImageIndex));
     lastRoundThird=thirdProductImageIndex;
 
+  
 
     console.log( Product.prototype.allProducts[firstProductImageIndex].source);
     firstProductImage.src= Product.prototype.allProducts[firstProductImageIndex].source;
     secondProductImage.src = Product.prototype.allProducts[secondProductImageIndex].source;
     thirdProductImage.src = Product.prototype.allProducts[thirdProductImageIndex].source;
     
-
+    Product.prototype.allProducts[firstProductImageIndex].shown++;
+    Product.prototype.allProducts[secondProductImageIndex].shown++;
+    Product.prototype.allProducts[thirdProductImageIndex].shown++;}}
 
     
   
